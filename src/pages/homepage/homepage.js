f87711713538a204a5f4026d2aeeb5b7fe9a5334
Kevin Smith <smit1985@msu.edu>
2023-04-14 23:22:12 -0400

    Updated analytics

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -10,22 +10,30 @@ import Spacer from "../../components/spacer/spacer"
 
 
 
-export default function HomePage(){
-    return(
+export default function HomePage() {
+    return (
         <div className="hp--container">
-             <Helmet>
+            <Helmet>
                 {/* <title>Homepage</title> */}
                 <meta charset="utf-8" name="Homepage" content="Welcome to Special Additions Construction." />
-                <link rel="canonical" href="/"/>
+                <link rel="canonical" href="/" />
                 <title>Special Additions Construction</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-                <meta name="description" content="Roofing, siding, and trim company with over 35 years of experience. Special Additions Construction maintains a safe on-site reputation."/>
-                <meta name="keywords" content="construction, roofing, Troy, Michigan, trim, special additions construction, troy roofing, troy trim, michigan roofing, michigan construction, affordable roofing, affordable roofers, local roofing contractor, michigan roofing contractor, roofing quote, siding quote, trim quote, michigan siding"/>
-                <meta property="og:title" content="Special Additions Construction Contractors"/>
-                <meta property="og:site_name" content="Special Additions Construction"/>
-                <meta property="og:url" content="https://saconst.com"/>
-                <meta property="og:description" content="Roofing, siding, and trim company with over 35 years of experience. Special Additions Construction maintains a safe on-site reputation."/>
-                <meta property="og:type" content="business.business"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+                <meta name="description" content="Roofing, siding, and trim company with over 35 years of experience. Special Additions Construction maintains a safe on-site reputation." />
+                <meta name="keywords" content="construction, roofing, Troy, Michigan, trim, special additions construction, troy roofing, troy trim, michigan roofing, michigan construction, affordable roofing, affordable roofers, local roofing contractor, michigan roofing contractor, roofing quote, siding quote, trim quote, michigan siding" />
+                <meta property="og:title" content="Special Additions Construction Contractors" />
+                <meta property="og:site_name" content="Special Additions Construction" />
+                <meta property="og:url" content="https://saconst.com" />
+                <meta property="og:description" content="Roofing, siding, and trim company with over 35 years of experience. Special Additions Construction maintains a safe on-site reputation." />
+                <meta property="og:type" content="business.business" />
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-JRG9N0HTDF"></script>
+                <script>
+                {`window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-JRG9N0HTDF');`}
+                </script>
                 {/* <meta property="og:image" content="https://saconst.com/3.jpg"/> */}
             </Helmet>
             <Hero />
